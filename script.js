91cd3179d55beff1a0a45140da81dff42b8acd2d
chm <carloshrmoraes@gmail.com>
2021-04-29 09:15:39 -0300

    library table

--- script.js
+++ script.js
@@ -1,33 +1,42 @@
 let myLibrary = [];
-const mainBooks = document.querySelector('div', '#main-Books');
-
 /* eslint-disable require-jsdoc */
-function Books(title, author, pages, wasRead) {
-  this.title = title;
-  this.author = author;
-  this.pages = pages;
-  this.wasRead = wasRead;
+class Books {
+  constructor(title, author, pages, read) {
+    this.Title = title;
+    this.Author = author;
+    this.Pages = pages;
+    this.Read = read;
+  }
+  get wasRead() {
+    return this.Read ? 'Already read.' : 'Still unread.';
+  }
 }
 
-Books.prototype.info = function () {
-  let read = this.wasRead ? 'Already read.' : 'Not read yet.';
-  return `${this.title}, by ${this.author}. ${this.pages} pages. ${read}`;
-};
+// Books.prototype.info = function () {
+//   return `${this.title}, by ${this.author}. ${this.pages} pages. ${this.wasRead}`;
+// };
 
 function addToLibrary(book) {
   myLibrary.push(book);
 }
 
 function viewLibrary() {
-  myLibrary.forEach(function (aBook) {
-    let books = null;
-    aBook.info();
-    books = document.createElement('p');
-    books.textContent = aBook.info();
-    mainBooks.appendChild(books);
+  myLibrary.forEach(function (singleBook) {
+    let books = document.createElement('tr');
+    tableProperties.innerHTML = '';
+    Object.entries(singleBook).forEach((bookData) => {
+      let props = document.createElement('td');
+      let dataBody = document.createElement('td');
+      props.textContent = bookData[0];
+      dataBody.textContent =
+        bookData[0] === 'Read' ? singleBook.wasRead : bookData[1];
+      books.appendChild(dataBody);
+      tableProperties.appendChild(props);
+    });
+    tableBody.appendChild(books);
   });
 }
 
 const hobbit = new Books('The Hobbit', 'J.R.R Tolkien', 300, false);
-
-console.log(hobbit.info());
+addToLibrary(hobbit);
+viewLibrary();
